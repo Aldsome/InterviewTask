0c7fb9ddf61a264831bc8c1f79a42d1969703df4
Aldsome <129302445+Aldsome@users.noreply.github.com>
2023-03-30 15:57:33 +0800

    Update search.py

--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,31 @@ def search_users(args):
     """
 
     # Implement search here!
+users = []
+
+    # check if the id parameter is provided and include the user with that id
+    if 'id' in args:
+        for user in USERS:
+            if user['id'] == args['id']:
+                users.append(user)
+                return users
+
+    for user in USERS:
+        # check if the name parameter is provided and partially matched
+        if 'name' in args and args['name'].lower() in user['name'].lower():
+            users.append(user)
+            continue
+        
+        # check if the age parameter is provided and matched with the range of age - 1 to age + 1
+        if 'age' in args and int(args['age'])-1 <= user['age'] <= int(args['age'])+1:
+            users.append(user)
+            continue
+        
+        # check if the occupation parameter is provided and partially matched
+        if 'occupation' in args and args['occupation'].lower() in user['occupation'].lower():
+            users.append(user)
+
+    # remove duplicates from the users list
+    users = list({user['id']: user for user in users}.values())
 
     return USERS
